6922233a59df0945ba26028f5fbb4677e0eff7fc
AhmedSaid25 <117524942+AhmedSaid25@users.noreply.github.com>
2022-12-01 17:15:43 +0200

    edit in one req

--- src/Service_Data.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class Service_Data {
-    HashMap<String, ArrayList<String>>data=new HashMap<String, ArrayList<String>>();
-    Services x=new Services();
-}
