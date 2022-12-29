1a4c9e1b1918e9bb4d430ff1997b3fd5ee0e9453
AhmedSaid25 <117524942+AhmedSaid25@users.noreply.github.com>
2022-12-29 23:25:25 +0200

    ph2

---
+++ src/main/java/com/example/demo/model/Refund.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+public class Refund {
+    String Name="";
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
